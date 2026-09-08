b5d8f1911e222dc236bf4947314287ad6019d470
Enzo Nunes Lopes <nuneslopesenzo02@gmail.com>
2026-09-08 20:59:26 -0300

    Update Estacao.cs

--- LojaCarga/Estacao.cs
+++ LojaCarga/Estacao.cs
@@ -3,7 +3,50 @@ namespace LojaCarga
 {
     class Estacao
     {
-        //propriedades (ATRIBUTOS)
+        //propriedades (ATRIBUTOS)namespace LojaCarga
+{
+    public class Estacao
+    {
+        // PROPRIEDADES (ATRIBUTOS)
+
+        public string? Motorista { get; set; }
+
+        public double CapacidadeKWh { get; set; }
+
+        public double PorcentagemAtual { get; set; }
+
+        public double PotenciaKw { get; set; }
+
+
+        // MÉTODOS (AÇÕES)
+
+        // MÉTODO PARA CALCULAR KW NECESSÁRIOS PARA CARREGAR A BATERIA
+        public double CalcularKWNecessarios()
+        {
+            double porcentagemFaltante = (100.00 - PorcentagemAtual) / 100.0;
+            return CapacidadeKWh * porcentagemFaltante;
+        }
+
+        // MÉTODO PARA CALCULAR TEMPO / HORAS NECESSÁRIAS
+        public double CalcularTempoHoras()
+        {
+            return CalcularKWNecessarios() / PotenciaKw;
+        }
+
+        // MÉTODO PARA SABER O VALOR DO CARREGAMENTO
+        public double CalcularValor()
+        {
+            const double PRECO_POR_KWH = 2.50;
+            return CalcularKWNecessarios() * PRECO_POR_KWH;
+        }
+
+        // MÉTODO PARA SABER SE O CARREGAMENTO É RÁPIDO
+        public bool CarregamentoRapido()
+        {
+            return (PotenciaKw >= 50.0) && (CalcularTempoHoras() < 1.0);
+        }
+    }
+}
         
         public string ?Motorista { get; set; }
 
